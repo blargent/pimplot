c8b16e0c37a44b2dd56aeabafcef210591d59763
Benjamin Largent <benjamin.largent@gmail.com>
2016-02-03 18:50:28 -0700

    back to normal for scroll test ios9 and 8 looks good now

--- resources/views/lot_master_beta.blade.php
+++ resources/views/lot_master_beta.blade.php
@@ -276,43 +276,6 @@
                             <input type="text" class="form-control" id="lot-history-num" aria-disabled="true" disabled="disabled">
                         </div>
 
-                        <div class="form-group">
-                            {{--<span class="glyphicon glyphicon-th-large" aria-hidden="true"></span>--}}
-                            {{--Data History Count--}}
-                            <label for="lot-history-num" class="control-label">Data history count for this lot: </label>&nbsp;<a href="javascript:alert('This will display history log here');">View history log</a>
-                            <input type="text" class="form-control" id="lot-history-num" aria-disabled="true" disabled="disabled">
-                        </div>
-                        <div class="form-group">
-                            {{--<span class="glyphicon glyphicon-th-large" aria-hidden="true"></span>--}}
-                            {{--Data History Count--}}
-                            <label for="lot-history-num" class="control-label">Data history count for this lot: </label>&nbsp;<a href="javascript:alert('This will display history log here');">View history log</a>
-                            <input type="text" class="form-control" id="lot-history-num" aria-disabled="true" disabled="disabled">
-                        </div><br ><hr>
-                        <div class="form-group">
-                            {{--<span class="glyphicon glyphicon-th-large" aria-hidden="true"></span>--}}
-                            {{--Data History Count--}}
-                            <label for="lot-history-num" class="control-label">Data history count for this lot: </label>&nbsp;<a href="javascript:alert('This will display history log here');">View history log</a>
-                            <input type="text" class="form-control" id="lot-history-num" aria-disabled="true" disabled="disabled">
-                        </div>
-                        <div class="form-group">
-                            {{--Notes--}}
-                            <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
-                            <label for="lot-notes" class="control-label">Notes:</label>
-                            <textarea class="form-control" id="lot-notes"></textarea>
-                        </div>
-                        <div class="form-group">
-                            {{--Notes--}}
-                            <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
-                            <label for="lot-notes" class="control-label">Notes:</label>
-                            <textarea class="form-control" id="lot-notes"></textarea>
-                        </div>
-                        <div class="form-group">
-                            {{--Notes--}}
-                            <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
-                            <label for="lot-notes" class="control-label">Notes:</label>
-                            <textarea class="form-control" id="lot-notes"></textarea>
-                        </div>
-
                     </form>
                 </div>
                 <div class="modal-footer">
