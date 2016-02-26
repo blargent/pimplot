a6cd0e6e0473c64dc8c6efcac33afe4a21150b33
Benjamin Largent <benjamin.largent@gmail.com>
2016-02-26 13:12:22 -0700

    trying to fix prod errors related to ReflectionClass errors

--- app/Http/Controllers/DataTablesController.php
+++ app/Http/Controllers/DataTablesController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
-use Yajra\Datatables\Datatables;
+//use Yajra\Datatables\Datatables;
 
 class DataTablesController extends Controller
 {
@@ -19,11 +19,11 @@ class DataTablesController extends Controller
 
 //$lots = DB::table('lot_defs')->select('id')->where('map_id', 2)->distinct()->pluck('id');
 
-    public function anyData() {
-        return Datatables::of(LotDef::query()
-            ->where('map_id', 2)
-            ->with('latestlotinfo', 'latestlotinfo.statusdef', 'latestlotinfo.user', 'latestlotinfo.buildtype'))
-            ->make(true);
-//        return Datatables::of(LotInfo::query())->make(true);
-    }
+//    public function anyData() {
+//        return Datatables::of(LotDef::query()
+//            ->where('map_id', 2)
+//            ->with('latestlotinfo', 'latestlotinfo.statusdef', 'latestlotinfo.user', 'latestlotinfo.buildtype'))
+//            ->make(true);
+////        return Datatables::of(LotInfo::query())->make(true);
+//    }
 }
--- resources/views/newreports/index.blade.php
+++ resources/views/newreports/index.blade.php
@@ -22,29 +22,29 @@
 @stop
 
 @push('scripts')
-<script>
-    $(function() {
-        $('#lot_defs-table').DataTable({
-            processing: true,
-            serverSide: true,
-            ajax: '{!! route('datatables.data') !!}',
-            columns: [
-                { data: 'latestlotinfo.lot_num', name: 'lot_num' },
-                { data: 'latestlotinfo.lot_name', name: 'lot_name' },
-                { data: 'latestlotinfo.statusdef.label', name: 'status_id' },
-                { data: 'latestlotinfo.buildtype.label', name: 'build_type_id' },
-//                { data: 'build_type_id', name: 'build_type_id' },
-                { data: 'latestlotinfo.fv_install_date', name: 'fv_install_date' },
-                { data: 'latestlotinfo.builder_date', name: 'builder_date' },
-                { data: 'latestlotinfo.adjust_date_to', name: 'adjust_date_to' },
-                { data: 'latestlotinfo.critical_issue_flag', name: 'critical_issue_flag' },
-                { data: 'latestlotinfo.verify_no_update', name: 'verify_no_update' },
-                { data: 'latestlotinfo.notes', name: 'notes' },
-                { data: 'latestlotinfo.created_at', name: 'created_at' },
-                { data: 'latestlotinfo.user.name', name: 'user_id' }
-            ]
-        });
-    });
+{{--<script>--}}
+    {{--$(function() {--}}
+        {{--$('#lot_defs-table').DataTable({--}}
+            {{--processing: true,--}}
+            {{--serverSide: true,--}}
+            {{--ajax: '{!! route('datatables.data') !!}',--}}
+            {{--columns: [--}}
+                {{--{ data: 'latestlotinfo.lot_num', name: 'lot_num' },--}}
+                {{--{ data: 'latestlotinfo.lot_name', name: 'lot_name' },--}}
+                {{--{ data: 'latestlotinfo.statusdef.label', name: 'status_id' },--}}
+                {{--{ data: 'latestlotinfo.buildtype.label', name: 'build_type_id' },--}}
+{{--//                { data: 'build_type_id', name: 'build_type_id' },--}}
+                {{--{ data: 'latestlotinfo.fv_install_date', name: 'fv_install_date' },--}}
+                {{--{ data: 'latestlotinfo.builder_date', name: 'builder_date' },--}}
+                {{--{ data: 'latestlotinfo.adjust_date_to', name: 'adjust_date_to' },--}}
+                {{--{ data: 'latestlotinfo.critical_issue_flag', name: 'critical_issue_flag' },--}}
+                {{--{ data: 'latestlotinfo.verify_no_update', name: 'verify_no_update' },--}}
+                {{--{ data: 'latestlotinfo.notes', name: 'notes' },--}}
+                {{--{ data: 'latestlotinfo.created_at', name: 'created_at' },--}}
+                {{--{ data: 'latestlotinfo.user.name', name: 'user_id' }--}}
+            {{--]--}}
+        {{--});--}}
+    {{--});--}}
 </script>
 @endpush
 
